e22bb90416b9e4173490f5524e4b8c1a56770e7e
Sebastian <seba.binaghi96@gmail.com>
2022-07-19 22:12:24 -0300

    primer entrega

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,17 +10,17 @@ const Footer = () => {
                 </li>
                 
                 <li className="Navli">
-                    <a className="link" href="_blank">Whatsapp</a>
+                    <a className="link" href="" >Whatsapp</a>
                     <img src="https://es.logodownload.org/wp-content/uploads/2018/10/whatsapp-logo-11.png" alt="logo" width={40}></img>
                 </li>
 
                 <li className="Navli">
-                    <a className="link" href="_blank">Instagram</a>
+                    <a className="link" href="https://www.instagram.com/?hl=es-la" >Instagram</a>
                     <img src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png" alt="logo" width={40}></img>
                 </li>
 
                 <li className="Navli">
-                    <a className="link" href="_blank">Facebook</a>
+                    <a className="link" href="https://www.facebook.com/">Facebook</a>
                     <img src="https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-3-1.png" alt="logo" width={40}></img>
                 </li>
 
